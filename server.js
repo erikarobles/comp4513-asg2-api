0c2a4b9f3cc152e594b31c61805b9451cc01a181
Erika Robles <erikarobles2551@gmail.com>
2023-04-03 23:53:54 -0600

    starting files

---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config(); 
+const express = require('express'); 
+const app = express(); 
+
+// serves up static files from the public folder. 
+app.use(express.static('public'));
+// also add a path to static 
+app.use('/static', express.static('public'));
+
+// get our data model 
+const Book = require('./models/Book'); 
+
+// tell node to use json and HTTP header features in body-parser 
+app.use(express.urlencoded({extended: true})); 
+// use the route handlers 
+
+// create connection to database 
+require('./handlers/dataConnector.js').connect(); 
+
+app.use(function (req, res, next) { 
+    res.status(404).send("Sorry can't find that!") 
+}); 
+
+const port = process.env.port; 
+app.listen(port, () => { 
+ console.log("Server running at port= " + port); 
+}); 
